ba9f669a084c419206d770a5d91c4bdb47902a13
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-09-10 19:50:53 +0300

    fix cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,9 +50,8 @@ export const login = async (req, res, next) => {
     const { password, ...info } = user._doc; //password = password, the rest of the "user" information is passed to the "info"
     res
       .cookie("accessToken", token, {
-        sameSite: "strict",
         httpOnly: true,
-        secure: true
+        
       })
       .status(200)
       .send(info);
@@ -75,9 +74,9 @@ export const getEmail = async (req, res) => {
 export const logout = async (req, res) => {
   res
     .clearCookie("accessToken", {
-      sameSite: "strict",
+      
         httpOnly: true,
-        secure: true
+        
     })
     .status(200)
     .send("User has been logged out.");
