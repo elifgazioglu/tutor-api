b39835c7a165adf4e9411a7bcbf93fbeaa693891
elifgazioglu <elifhusna.gazioglu21@ogr.atauni.edu.tr>
2023-09-10 19:34:46 +0300

    fix cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,6 +51,7 @@ export const login = async (req, res, next) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        sameSite: "none"
       })
       .status(200)
       .send(info);
